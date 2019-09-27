d67d8e668d78560eb90a837680dc8c66155ed51c
arnabbhat82 <arnabbhat@gmail.com>
2019-09-27 17:32:17 +0530

    unsubscribing subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserServiceService } from './user-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   userActivated = false;
+  private activatedSub: Subscription;
   constructor(
     private router: Router,
     private userService: UserServiceService,
   ) {}
   title = 'closer-core-of-observables';
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe(data => {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(data => {
       this.userActivated = data;
     });
+  }
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe();
   }
   goToParent() {
     this.router.navigate(['parent']);
